2fa5ce6c87aee1612dad8b0bbb7e260fa2498c6f
MarcusQuirino <marcusfilipus@gmail.com>
2024-08-31 00:25:03 -0300

    wip: get user by clerk id

---
+++ src/lib/getUserByClerkId.ts
@@ -0,0 +1,12 @@
+// import "server-only";
+
+// import { db } from "@/server/db";
+// import { users } from "@/server/db/schema";
+// import { eq } from "drizzle-orm";
+
+// export async function getUserByClerkId(clerkId: string) {
+//   const user = await db.query.users.findFirst({
+//     where: eq(users.clerkId, clerkId),
+//   });
+//   return user;
+// }
